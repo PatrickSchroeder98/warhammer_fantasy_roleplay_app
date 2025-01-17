570379707c0d412e6f649fc3d00e474a19c0ba81
Patrick Schroder <patrickschroder479@gmail.com>
2025-01-17 20:15:46 +0100

    Updated views and forms to include more models
    Updated create character view and forms to include more fields from various models.

--- wfra/characters/forms.py
+++ wfra/characters/forms.py
@@ -1,11 +1,12 @@
 from django import forms
-from .models import Characters
+from .models import Characters, Fate, Resilience
 
 
 class CharactersForm(forms.ModelForm):
     class Meta:
         model = Characters
-        fields = ('name', 'species')  # Fields to include in the form
+        fields = ('name', 'species', 'character_class', 'career', 'career_tier', 'career_path', 'status', 'age',
+                  'height', 'hair', 'eyes')  # Fields to include in the form
 
         # Optionally customize widgets or labels
         widgets = {
@@ -16,3 +17,16 @@ class CharactersForm(forms.ModelForm):
             'name': 'Character Name',
             'species': 'Species',
         }
+
+
+class FateForm(forms.ModelForm):
+    class Meta:
+        model = Fate
+        fields = ('fate', 'fortune')
+
+
+class ResilienceForm(forms.ModelForm):
+    class Meta:
+        model = Resilience
+        fields = ('resilience', 'resolve', 'motivation')
+
--- wfra/characters/views.py
+++ wfra/characters/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.views import View
 from . import models
-from .forms import CharactersForm
+from .forms import CharactersForm, FateForm, ResilienceForm
 
 
 class ListCharacters(LoginRequiredMixin, generic.ListView):
@@ -14,16 +15,60 @@ class ListCharacters(LoginRequiredMixin, generic.ListView):
         return models.Characters.objects.filter(user=self.request.user)
 
 
-class CreateCharacter(LoginRequiredMixin, generic.CreateView):
-    model = models.Characters
-    form_class = CharactersForm
-    success_url = reverse_lazy('characters:all')
+# class CreateCharacter(LoginRequiredMixin, generic.CreateView):
+#     model = models.Characters
+#     form_class = CharactersForm
+#     success_url = reverse_lazy('characters:all')
+#
+#     def form_valid(self, form):
+#         self.object = form.save(commit=False)
+#         self.object.user = self.request.user
+#         self.object.save()
+#         return super().form_valid(form)
 
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.user = self.request.user
-        self.object.save()
-        return super().form_valid(form)
+class CreateCharacter(LoginRequiredMixin, View):
+    def get(self, request):
+        # Create empty forms
+        character_form = CharactersForm()
+        fate_form = FateForm()
+        resilience_form = ResilienceForm()
+        return render(request, "characters/characters_form.html", {
+            "character_form": character_form,
+            "fate_form": fate_form,
+            "resilience_form": resilience_form,
+        })
+
+    def post(self, request):
+        """Method binds submitted data to the forms. Then it validates all forms. If they are valid, it saves the data.
+        If they are invalid, it re-renders the form with errors."""
+        character_form = CharactersForm(request.POST)
+        fate_form = FateForm(request.POST)
+        resilience_form = ResilienceForm(request.POST)
+
+        if character_form.is_valid() and fate_form.is_valid() and resilience_form.is_valid():
+            # Save the main character form
+            character = character_form.save(commit=False)
+            character.user = request.user  # Set the logged-in user
+            character.save()
+
+            # Save the related forms with a reference to the character
+            fate = fate_form.save(commit=False)
+            fate.character_id = character
+            fate.save()
+
+            resilience = resilience_form.save(commit=False)
+            resilience.character_id = character
+            resilience.save()
+
+            messages.success(request, "Character created successfully!")
+            return redirect("characters:all")
+
+        # If any form is invalid, re-render the form with errors
+        return render(request, "characters/character_form.html", {
+            "character_form": character_form,
+            "fate_form": fate_form,
+            "resilience_form": resilience_form,
+        })
 
 
 class DetailViewCharacter(LoginRequiredMixin, generic.DetailView):
