7cf3f9623698f2865492d835acfbc611164164f0
Patrick Schroder <patrickschroder479@gmail.com>
2025-01-22 11:17:47 +0100

    Updated methods in views.py
    Updated methods to save all data from form to database, updated detail view contexts.

--- wfra/characters/views.py
+++ wfra/characters/views.py
@@ -71,13 +71,33 @@ class CreateCharacter(LoginRequiredMixin, View):
         """Method binds submitted data to the forms. Then it validates all forms. If they are valid, it saves the data.
         If they are invalid, it re-renders the form with errors."""
         character_form = CharactersForm(request.POST)
+        characteristics_form = CharacteristicsForm(request.POST)
         fate_form = FateForm(request.POST)
         resilience_form = ResilienceForm(request.POST)
+        basic_skills_form = BasicSkillsForm(request.POST)
+        advanced_skills_form = AdvancedSkillsForm(request.POST)
+        talents_form = TalentsForm(request.POST)
+        ambitions_form = AmbitionsForm(request.POST)
+        party_form = PartyForm(request.POST)
+        psychology_form = PsychologyForm(request.POST)
+        corruption_and_mutation_form = CorruptionAndMutationForm(request.POST)
+        wounds_form = WoundsForm(request.POST)
+        sin_form = SinForm(request.POST)
 
         if (
             character_form.is_valid()
+            and characteristics_form.is_valid()
             and fate_form.is_valid()
             and resilience_form.is_valid()
+            and basic_skills_form.is_valid()
+            and advanced_skills_form.is_valid()
+            and talents_form.is_valid()
+            and ambitions_form.is_valid()
+            and party_form.is_valid()
+            and psychology_form.is_valid()
+            and corruption_and_mutation_form.is_valid()
+            and wounds_form.is_valid()
+            and sin_form.is_valid()
         ):
             # Save the main character form
             character = character_form.save(commit=False)
@@ -85,6 +105,10 @@ class CreateCharacter(LoginRequiredMixin, View):
             character.save()
 
             # Save the related forms with a reference to the character
+            characteristics = characteristics_form.save(commit=False)
+            characteristics.character_id = character
+            characteristics.save()
+
             fate = fate_form.save(commit=False)
             fate.character_id = character
             fate.save()
@@ -92,6 +116,38 @@ class CreateCharacter(LoginRequiredMixin, View):
             resilience = resilience_form.save(commit=False)
             resilience.character_id = character
             resilience.save()
+
+            basic_skills = basic_skills_form.save(commit=False)
+            basic_skills.character_id = character
+            basic_skills.save()
+
+            advanced_skills = advanced_skills_form.save(commit=False)
+            advanced_skills.character_id = character
+            advanced_skills.save()
+
+            talents = talents_form.save(commit=False)
+            talents.character_id = character
+            talents.save()
+
+            party = party_form.save(commit=False)
+            party.character_id = character
+            party.save()
+
+            psychology = psychology_form.save(commit=False)
+            psychology.character_id = character
+            psychology.save()
+
+            corruption_and_mutation = corruption_and_mutation_form.save(commit=False)
+            corruption_and_mutation.character_id = character
+            corruption_and_mutation.save()
+
+            wounds = wounds_form.save(commit=False)
+            wounds.character_id = character
+            wounds.save()
+
+            sin = sin_form.save(commit=False)
+            sin.character_id = character
+            sin.save()
 
             messages.success(request, "Character created successfully!")
             return redirect("characters:all")
@@ -102,8 +158,18 @@ class CreateCharacter(LoginRequiredMixin, View):
             "characters/character_form.html",
             {
                 "character_form": character_form,
+                "characteristics_form": characteristics_form,
                 "fate_form": fate_form,
                 "resilience_form": resilience_form,
+                "basic_skills_form": basic_skills_form,
+                "advanced_skills_form": advanced_skills_form,
+                "talents_form": talents_form,
+                "ambitions_form": ambitions_form,
+                "party_form": party_form,
+                "psychology_form": psychology_form,
+                "corruption_and_mutation_form": corruption_and_mutation_form,
+                "wounds_form": wounds_form,
+                "sin_form": sin_form,
             },
         )
 
@@ -116,13 +182,26 @@ class DetailViewCharacter(LoginRequiredMixin, generic.DetailView):
         context = super().get_context_data(**kwargs)
         character = self.object
 
-        context["experience"] = models.Experience.objects.filter(character_id=character)
-        context["resilience"] = models.Resilience.objects.filter(
+        context["characteristics"] = models.Characteristics.objects.filter(
+            character_id=character
+        )
+        context["fate"] = models.Fate.objects.filter(character_id=character)
+        context["resilience"] = models.Resilience.objects.filter(character_id=character)
+        context["basic_skills"] = models.BasicSkills.objects.filter(
             character_id=character
         )
         context["advanced_skills"] = models.AdvancedSkills.objects.filter(
             character_id=character
         )
+        context["talents"] = models.Talents.objects.filter(character_id=character)
+        context["ambitions"] = models.Ambitions.objects.filter(character_id=character)
+        context["party"] = models.Party.objects.filter(character_id=character)
+        context["psychology"] = models.Psychology.objects.filter(character_id=character)
+        context["corruption_and_mutation"] = (
+            models.CorruptionAndMutation.objects.filter(character_id=character)
+        )
+        context["wounds"] = models.Wounds.objects.filter(character_id=character)
+        context["sin"] = models.Sin.objects.filter(character_id=character)
 
         return context
 
