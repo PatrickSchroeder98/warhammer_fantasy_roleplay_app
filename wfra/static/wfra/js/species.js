ecb0de8a26e7e280f0b30a60846b0b2eedecdeff
Patrick Schroder <patrickschroder479@gmail.com>
2025-03-12 15:45:12 +0100

    Finished classes lists
    Finished developing lists in dictionary of classes to automatization script.

--- wfra/static/wfra/js/species.js
+++ wfra/static/wfra/js/species.js
@@ -6,27 +6,47 @@ document.addEventListener("DOMContentLoaded", function () {
         "Human": ["Apothecary", "Engineer", "Lawyer", "Nun", "Physician", "Priest", "Scholar", "Wizard",
                   "Agitator", "Artisan", "Beggar", "Investigator", "Merchant", "Rat Catcher", "Townsman", "Watchman",
                   "Advisor", "Artist", "Duellist", "Envoy", "Noble", "Servant", "Spy", "Warden",
-                  "Bailiff", "Hedge Witch", "Herbalist", "Hunter", "Miner", "Mystic", "Scout", "Villager"],
+                  "Bailiff", "Hedge Witch", "Herbalist", "Hunter", "Miner", "Mystic", "Scout", "Villager",
+                  "Bounty Hunter", "Coachman", "Entertainer", "Flagellant", "Messenger", "Pedlar", "Roadwarden", "Witch Hunter",
+                  "Boatman", "Huffer", "Riverwarden", "Riverwoman", "Seaman", "Smuggler", "Stevedore", "Wrecker",
+                  "Bawd", "Charlatan", "Fence", "Grave Robber", "Outlaw", "Racketeer", "Thief", "Witch",
+                  "Cavalryman", "Guard", "Knight", "Pit Fighter", "Protagonist", "Soldier", "Warrior Priest"],
 
         "Dwarf": ["Apothecary", "Engineer", "Lawyer", "Physician", "Scholar",
                   "Agitator", "Artisan", "Beggar", "Investigator", "Merchant", "Rat Catcher", "Townsman", "Watchman",
                   "Advisor", "Artist", "Duellist", "Envoy", "Noble", "Servant", "Spy", "Warden",
-                  "Bailiff", "Hunter", "Miner", "Scout", "Villager"],
+                  "Bailiff", "Hunter", "Miner", "Scout", "Villager",
+                  "Bounty Hunter", "Coachman", "Entertainer", "Messenger", "Pedlar",
+                  "Boatman", "Huffer", "Riverwoman", "Seaman", "Smuggler", "Stevedore", "Wrecker",
+                  "Fence", "Outlaw", "Racketeer", "Thief",
+                  "Guard", "Pit Fighter", "Protagonist", "Soldier", "Slayer"],
 
         "Halfling": ["Apothecary", "Engineer", "Lawyer", "Physician", "Scholar",
                      "Agitator", "Artisan", "Beggar", "Investigator", "Merchant", "Rat Catcher", "Townsman", "Watchman",
                      "Advisor", "Artist", "Envoy", "Servant", "Spy", "Warden",
-                     "Bailiff", "Herbalist", "Hunter", "Miner", "Scout", "Villager"],
+                     "Bailiff", "Herbalist", "Hunter", "Miner", "Scout", "Villager",
+                     "Bounty Hunter", "Coachman", "Entertainer", "Messenger", "Pedlar", "Roadwarden",
+                     "Boatman", "Huffer", "Riverwarden", "Riverwoman", "Seaman", "Smuggler", "Stevedore",
+                     "Bawd", "Charlatan", "Fence", "Grave Robber", "Outlaw", "Racketeer", "Thief",
+                     "Guard", "Pit Fighter", "Soldier"],
 
         "High Elf": ["Apothecary", "Lawyer", "Physician", "Scholar", "Wizard",
                      "Artisan", "Investigator", "Merchant", "Townsman", "Watchman",
                      "Advisor", "Artist", "Duellist", "Envoy", "Noble", "Spy", "Warden",
-                     "Herbalist", "Hunter", "Scout"],
+                     "Herbalist", "Hunter", "Scout",
+                     "Bounty Hunter", "Entertainer", "Messenger",
+                     "Boatman", "Seaman", "Smuggler",
+                     "Bawd", "Charlatan", "Outlaw",
+                     "Cavalryman", "Guard", "Knight", "Pit Fighter", "Protagonist", "Soldier"],
 
         "Wood Elf": ["Scholar", "Wizard",
                      "Artisan",
                      "Advisor", "Artist", "Envoy", "Noble", "Spy", "Warden",
-                     "Herbalist", "Hunter", "Mystic", "Scout"]
+                     "Herbalist", "Hunter", "Mystic", "Scout",
+                     "Bounty Hunter", "Entertainer", "Messenger",
+                     "Wrecker",
+                     "Outlaw",
+                     "Cavalryman", "Guard", "Knight", "Pit Fighter", "Soldier"]
 
     };
 
