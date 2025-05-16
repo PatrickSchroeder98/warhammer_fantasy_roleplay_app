1fd56bb21fcccced9347890d5ecad276b97f71b6
Patrick Schroder <patrickschroder479@gmail.com>
2025-05-16 12:58:24 +0200

    Added detail views and path in equipment
    Added detail views of every item type and path to each added detail view in urls.py.

--- wfra/equipment/urls.py
+++ wfra/equipment/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 from . import views
 
-app_name = 'characters'
+app_name = 'equipment'
 
 urlpatterns = [
     path("meleeweapons/<int:pk>/", views.DetailViewMeleeWeapons.as_view(), name="detail_melee_weapon"),
+    path("rangedweapons/<int:pk>/", views.DetailViewRangedWeapons.as_view(), name="detail_ranged_weapon"),
+    path("ammunition/<int:pk>/", views.DetailViewAmmunition.as_view(), name="detail_ammunition"),
+    path("armour/<int:pk>/", views.DetailViewArmour.as_view(), name="detail_armour"),
+    path("packsandcontainers/<int:pk>/", views.DetailViewPacksAndContainers.as_view(), name="detail_packs_and_containers"),
+    path("clothingandaccessories/<int:pk>/", views.DetailViewClothingAndAccessories.as_view(), name="detail_clothing_and_accessories"),
+    path("fooddrinkandlodging/<int:pk>/", views.DetailViewFoodDrinkAndLodging.as_view(), name="detail_food_drink_and_lodging"),
+    path("toolsandkits/<int:pk>/", views.DetailViewToolsAndKits.as_view(), name="detail_tools_and_kits"),
+    path("booksanddocuments/<int:pk>/", views.DetailViewBooksAndDocuments.as_view(), name="detail_books_and_documents"),
+    path("tradetoolsandworkshops/<int:pk>/", views.DetailViewTradeToolsAndWorkshops.as_view(), name="detail_trade_tools_and_workshops"),
+    path("animalsandvehicles/<int:pk>/", views.DetailViewAnimalsAndVehicles.as_view(), name="detail_animals_and_vehicles"),
+    path("drugsandpoisons/<int:pk>/", views.DetailViewDrugsAndPoisons.as_view(), name="detail_drugs_and_poisons"),
+    path("herbsanddraughts/<int:pk>/", views.DetailViewHerbsAndDraughts.as_view(), name="detail_herbs_and_draughts"),
+    path("prosthetics/<int:pk>/", views.DetailViewProsthetics.as_view(), name="detail_prosthetics"),
+    path("miscellaneoustrappings/<int:pk>/", views.DetailViewMiscellaneousTrappings.as_view(), name="detail_miscellaneous_trappings"),
+    path("hirelings/<int:pk>/", views.DetailViewHirelings.as_view(), name="detail_hirelings"),
 ]
--- wfra/equipment/views.py
+++ wfra/equipment/views.py
@@ -3,12 +3,115 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
 from .models import (
     MeleeWeapons,
-
+    RangedWeapons,
+    Ammunition,
+    Armour,
+    PacksAndContainers,
+    ClothingAndAccessories,
+    FoodDrinkAndLodging,
+    ToolsAndKits,
+    BooksAndDocuments,
+    TradeToolsAndWorkshops,
+    AnimalsAndVehicles,
+    DrugsAndPoisons,
+    HerbsAndDraughts,
+    Prosthetics,
+    MiscellaneousTrappings,
+    Hirelings,
 )
 
 
 class DetailViewMeleeWeapons(LoginRequiredMixin, DetailView):
-    """Detail view of a melee weapon."""
+    """Detail view of melee weapons."""
     model = MeleeWeapons
-    context_object_name = "meleeweapon"
+    context_object_name = "melee_weapon"
 
+
+class DetailViewRangedWeapons(LoginRequiredMixin, DetailView):
+    """Detail view of ranged weapons."""
+    model = RangedWeapons
+    context_object_name = "ranged_weapon"
+
+
+class DetailViewAmmunition(LoginRequiredMixin, DetailView):
+    """Detail view of ammunition."""
+    model = Ammunition
+    context_object_name = "ammunition"
+
+
+class DetailViewArmour(LoginRequiredMixin, DetailView):
+    """Detail view of an armour."""
+    model = Armour
+    context_object_name = "armour"
+
+
+class DetailViewPacksAndContainers(LoginRequiredMixin, DetailView):
+    """Detail view of packs and containers."""
+    model = PacksAndContainers
+    context_object_name = "packs_and_containers"
+
+
+class DetailViewClothingAndAccessories(LoginRequiredMixin, DetailView):
+    """Detail view of clothing and accessories."""
+    model = ClothingAndAccessories
+    context_object_name = "clothing_and_accessories"
+
+
+class DetailViewFoodDrinkAndLodging(LoginRequiredMixin, DetailView):
+    """Detail view of food drink and lodging."""
+    model = FoodDrinkAndLodging
+    context_object_name = "food_drink_and_lodging"
+
+
+class DetailViewToolsAndKits(LoginRequiredMixin, DetailView):
+    """Detail view of tools and kits."""
+    model = ToolsAndKits
+    context_object_name = "tools_and_kits"
+
+
+class DetailViewBooksAndDocuments(LoginRequiredMixin, DetailView):
+    """Detail view of books and documents."""
+    model = BooksAndDocuments
+    context_object_name = "books_and_documents"
+
+
+class DetailViewTradeToolsAndWorkshops(LoginRequiredMixin, DetailView):
+    """Detail view of trade tools and workshops."""
+    model = TradeToolsAndWorkshops
+    context_object_name = "trade_tools_and_workshops"
+
+
+class DetailViewAnimalsAndVehicles(LoginRequiredMixin, DetailView):
+    """Detail view of animals and vehicles."""
+    model = AnimalsAndVehicles
+    context_object_name = "animals_and_vehicles"
+
+
+class DetailViewDrugsAndPoisons(LoginRequiredMixin, DetailView):
+    """Detail view of drugs and poisons."""
+    model = DrugsAndPoisons
+    context_object_name = "drugs_and_poisons"
+
+
+class DetailViewHerbsAndDraughts(LoginRequiredMixin, DetailView):
+    """Detail view of herbs and draughts."""
+    model = HerbsAndDraughts
+    context_object_name = "herbs_and_draughts"
+
+
+class DetailViewProsthetics(LoginRequiredMixin, DetailView):
+    """Detail view of prosthetics."""
+    model = Prosthetics
+    context_object_name = "prosthetics"
+
+
+class DetailViewMiscellaneousTrappings(LoginRequiredMixin, DetailView):
+    """Detail view of miscellaneous trappings."""
+    model = MiscellaneousTrappings
+    context_object_name = "miscellaneous_trappings"
+
+
+class DetailViewHirelings(LoginRequiredMixin, DetailView):
+    """Detail view of hirelings."""
+    model = Hirelings
+    context_object_name = "hirelings"
